9604ea6cee923a91fb40644c0e86695af595f2ae
yujung7768903 <yu-jung31476@naver.com>
2022-03-27 23:59:26 +0900

    feat: Add email input box

--- src/components/feedback.vue
+++ src/components/feedback.vue
@@ -2,7 +2,10 @@
     <div id="feedback">
         <div class="black-bg"></div>
         <b-form ref="form" id="feedback-area" @submit.prevent="submitFeedback" @reset="resetFeedback">
-            <b-form-textarea name="feedback" vmodel="feedback" rows="5" placeholder="오류를 제보해주세요. 피드백은 언제나 환영합니다. 감사합니다:)">
+            <span>이메일</span>
+            <b-form-input type="email" placeholder="이메일을 남겨주시면, 오류 수정이 완료되었을 때 알려드립니다."></b-form-input>
+            <span>오류제보</span>
+            <b-form-textarea name="feedback" vmodel="feedback" rows="5" placeholder="피드백은 언제나 환영합니다. 감사합니다:)">
             </b-form-textarea>
             <div class="horizontal container center">
                 <b-button class="point box" type="submit">제출</b-button>
@@ -67,16 +70,20 @@ export default {
 #feedback-area {
   position: relative;
   width: 630px;
-  height: 200px;
+  /* height: 200px; */
   display: flex; 
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: left;
   gap: 10px;
   border-radius: 0.25rem;
-  padding: 10px;
+  padding: 20px;
   background: white;
   margin: auto;
   z-index: 5;
+}
+
+#feedback-area > span {
+    text-align: left;
 }
 </style>
